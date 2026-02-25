c04aae06b4be86f316c41dbdf2d5dd1c53b849d1
ondrejgurecky <ondrejgurecky@gmail.com>
2026-02-25 17:05:27 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import requests
 import pandas as pd
 
 # KONFIGURACE
-API_KEY = "RaguaOTlcINiC40Dir7Pwnjr-C2PqAAMBF5J6OUgM0M"  # Mapy.cz API
+API_KEY = st.secrets["MAPY_API"]
 START_ADDR = "Šátalská 469/1, Praha 4, 14100 Praha"
 
 # VOZIDLA uživatele
